196b7476d1a3459507a0f7fc898e583328a6067a
Kvijay <keertivijayananth06@gmail.com>
2024-12-14 13:19:28 +0530

    Setupfile

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'BoneSegmentation',
+    version= '0.0.0',
+    author= 'Keerti',
+    author_email= 'keertivijayananth06@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
